61985bb8b3ce8f7244c10eee3974032f5aaddd6a
Egor Demidov <mail@edemidov.com>
2024-10-17 12:02:37 -0400

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# If there is no venv, create it
+if [ ! -d venv ]
+then
+    python3 -m venv --system-site-packages ./venv
+fi
+
+# Activate the venv
+source ./venv/bin/activate
+
+# Install RenderCV
+pip3 install rendercv
+
---
+++ render.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Activate the venv
+source ./venv/bin/activate
+
+# Render the CV
+rendercv render Egor_Demidov_CV.yaml
+
