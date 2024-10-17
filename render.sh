5994c75d8b7367419d87c8d90c6d05ae0f9a4066
Egor Demidov <mail@edemidov.com>
2024-10-17 13:26:05 -0400

    Updated the CV

--- render.sh
+++ render.sh
@@ -3,6 +3,9 @@
 # Activate the venv
 source ./venv/bin/activate
 
+# Clear the previous output
+#rm -rf rendercv_output
+
 # Render the CV
-rendercv render Egor_Demidov_CV.yaml
+rendercv render -nomd -nohtml -nopng Egor_Demidov_CV.yaml
 
